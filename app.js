8074017f5c19390f8be396a9b42e16e68614c5cd
nabiladigo <nabiladigo91@gmail.com>
2021-12-17 16:45:25 -0500

    commit 1 - Verbal questions

---
+++ app.js
@@ -0,0 +1,2 @@
+// the **parameter** is a variable in function and the **arguments** are the actual  value of that parameter that get passed to function.
+// the **return** is used for returnig values from function and the **console.log** used to make sure the function works its only for debugging/informative purposes.
