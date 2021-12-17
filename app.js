1ff0fd18df29fb452e2cf42ab2252261f8e96750
nabiladigo <nabiladigo91@gmail.com>
2021-12-17 16:55:56 -0500

    calculateCube done

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // the **parameter** is a variable in function and the **arguments** are the actual  value of that parameter that get passed to function.
 // the **return** is used for returnig values from function and the **console.log** used to make sure the function works its only for debugging/informative purposes.
+function calculateCube(num){
+    return (num**3)
+}
+console.log(calculateCube(5))
